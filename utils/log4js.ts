ea3ced5341018125d33d1589a28f161be48b9c84
lushiyi <iwof920125@hotmail.com>
2019-09-17 11:36:53 +0800

    优化日志输出

--- utils/log4js.ts
+++ utils/log4js.ts
@@ -1,6 +1,30 @@
 import { Log4js, configure } from "log4js";
-import config = require("../../config.json");
-const log4js: Log4js = configure(config.log4js_config);
+const logId = process.env.NODE_APP_INSTANCE ? process.env.NODE_APP_INSTANCE : "0";
+const log4js: Log4js = configure({
+    pm2: true,
+    disableClustering: true,
+    appenders: {
+        out: {
+            type: "stdout"
+        },
+        server: {
+            type: "dateFile",
+            filename: `logs/${process.env.NODE_ENV}/${logId}`,
+            pattern: "yyyy-MM-dd.log",
+            category: "default",
+            alwaysIncludePattern: true
+        }
+    },
+    categories: {
+        default: {
+            appenders: [
+                "server",
+                "out"
+            ],
+            level: "DEBUG"
+        }
+    }
+});
 
 export default log4js;
 
