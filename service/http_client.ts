2d7d64856e470caa81a18254a301dd4c803682b8
jone <lk920125@hotmail.com>
2019-10-23 15:38:24 +0800

    增加日志

--- service/http_client.ts
+++ service/http_client.ts
@@ -104,6 +104,7 @@ export async function http_post_form_async(url: string, data: object, secret: st
         },
     };
 
+    console.log(opt);
     return new Promise((resolve, reject) => {
         post(url, opt, function (error, response, body) {
             if (error) reject(null);
