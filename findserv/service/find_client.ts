f82f302e251676f9da5fe09a564f939f1df8f297
lushiyi <iwof920125@hotmail.com>
2020-03-11 15:21:59 +0800

    导出 服务参数

--- findserv/service/find_client.ts
+++ findserv/service/find_client.ts
@@ -2,11 +2,10 @@ import log4js from "../../utils/log4js";
 import { basename } from "path";
 const logger = log4js.getLogger(basename(__filename));
 ///////////////////////////////////////////////////////
-// import { get_user_amount } from "../../manager/user_mgr";
 import { http_post_async } from "../../service/http_json";
 import { SERVER_REQUEST, ServerReq } from "../readme/httpApi_find";
 
-const SERVER_INFO: ServerReq = { server_type: "", server_id: "", tick_time: 0, http_ip: "", http_port: 0, ws_ip: "", ws_port: 0, load: 0, memory: "" };
+export const SERVER_INFO: ServerReq = { server_type: "", server_id: "", tick_time: 0, http_ip: "", http_port: 0, ws_ip: "", ws_port: 0, load: 0, memory: "" };
 
 export async function server_report_async(server_info: ServerReq, find_ip: string, find_port: number, find_tick_time: number) {
     Object.assign(SERVER_INFO, server_info);
