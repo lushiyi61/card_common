bdb2a6c9d311c5f28dc5d8ab27ff869db9b3881b
lushiyi <iwof920125@hotmail.com>
2020-03-11 15:44:46 +0800

    增加服务器交互方法

--- manager/server_mgr.ts
+++ manager/server_mgr.ts
@@ -4,7 +4,7 @@ const logger = log4js.getLogger(basename(__filename));
 ///////////////////////////////////////////////////////
 import config = require("../../config.json");
 import { FindReq, SERVER_REQUEST, ServerRes } from "../findserv/readme/httpApi_find";
-import { HttpReturn } from "../service/http_server";
+import { HttpReturn, ERROR_CODE } from "../service/http_server";
 import { http_post_async } from "../service/http_json";
 
 
@@ -38,4 +38,24 @@ export async function server_manager_start_async(server_types: string[]) {
 
 export function get_server_by_type(server_type: string): ServerRes {
     return SERVER_MAP_INFO.get(server_type);
+}
+
+export async function post_to_server_async(server_type: string, cmd: string, params: any): Promise<HttpReturn> {
+    const server = SERVER_MAP_INFO.get(server_type);
+    if (server) {
+        try {
+            const result: HttpReturn = await http_post_async(
+                server.http_ip,
+                server.http_port,
+                cmd,
+                params
+            )
+            return result;
+        } catch (error) {
+            logger.warn("服务器请求错误：[%s]", error);
+            return { code: ERROR_CODE.SERVER_TIME_OUT }
+        }
+    }
+    logger.warn("服务器不存在，类型[%s]", server_type);
+    return { code: ERROR_CODE.SERVER_NOT_EXIST }
 }
--- service/http_server.ts
+++ service/http_server.ts
@@ -4,6 +4,13 @@ export interface HttpReturn {
     data?: any,
 }
 
+export const ERROR_CODE = {
+    SUCCESS: "0",               // 执行成功
+    ERROR: "E5001",             // 客户端非法操作
+    SERVER_NOT_EXIST: "E5002",  // 服务器不存在
+    SERVER_TIME_OUT: "E5003",   // 服务器超时
+}
+
 export function http_return(res, ret: HttpReturn) {
     const httpReturn: HttpReturn = { code: "0", msg: "success" };
     Object.assign(httpReturn, ret);
